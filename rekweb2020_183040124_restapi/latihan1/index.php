057db967c298918dacd0ba097574bbbf2496a6ea
Naikawa <62163110+Naikawa@users.noreply.github.com>
2020-11-28 17:04:38 +0700

    Semoga benar AMIINNN
    Bismillah

---
+++ rekweb2020_183040124_restapi/latihan1/index.php
@@ -0,0 +1,46 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "", "universitas_pasundan");
+
+$result = $mysqli->query("SELECT * FROM mahasiswa");
+$rows = $result->fetch_all();
+
+
+$data_json = json_encode($rows);
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Rest API</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+
+<body class="container">
+    <h3 class="my-3">Daftar Mahasiswa</h3>
+    <table border="1" class="table table-hover">
+        <thead class="thead-dark">
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Nrp</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $i = 1; ?>
+            <?php foreach (json_decode($data_json) as $data) { ?>
+                <tr>
+                    <td><?= $i; ?></td>
+                    <td><?= $data[1]; ?></td>
+                    <td><?= $data[2]; ?></td>
+                    <td><?= $data[3]; ?></td>
+                </tr>
+                <?= $i++; ?>
+            <?php } ?>
+        </tbody>
+    </table>
+</body>
+
+</html>
